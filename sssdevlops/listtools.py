a127d7be307fc86934a31904a89a4575fc7cd62d
Xuyan Ru <lelielru@gmail.com>
2018-07-10 09:29:45 -0400

    updated code

--- sssdevlops/listtools.py
+++ sssdevlops/listtools.py
@@ -3,4 +3,25 @@ Miscelllaneous list manipulation
 """
 
 def split(num_lst, index):
-    pass #ok
+    '''
+
+    Parameters
+    ----------
+    num_lst: list
+    index: int
+
+    Returns
+    -------
+    Two lists, splitting num_lst by 'index'
+
+    Examples
+    --------
+    >>> split(['a','b','c','d'],3)
+    (['a','b','c'],['d'])
+    '''
+
+    list1 = num_lst[:index]
+    list2 = num_lst[index:]
+
+    return list1, list2
+
